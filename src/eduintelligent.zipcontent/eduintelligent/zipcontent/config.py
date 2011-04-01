bab3a1c461ee052be35917082b987e23c213c44c
tzicatl <tzicatl@gmail.com>
2011-03-31 18:52:59 -0600

    Añadir algo de integracion a zipcontent con cyn.in

--- src/eduintelligent.zipcontent/eduintelligent/zipcontent/config.py
+++ src/eduintelligent.zipcontent/eduintelligent/zipcontent/config.py
@@ -5,9 +5,8 @@ import Globals
 
 PROJECTNAME = "eduintelligent.zipcontent"
 
-#SCORM_STORE = os.path.join(Globals.INSTANCE_HOME, 'var', 'eduintelligent')
-#EXTERNA_URL = "http://lms.iservices.com.mx/scorm"  # Use mod_rewrite for this
+EXTERNAL_URL = "http://blufrog.iservices.com.mx/scorm"  # Use mod_rewrite for this
+#EXTERNAL_URL = "http://localhost/~erik/static"
 
-# for devel
-EXTERNAL_URL = "http://localhost/~erik/static"   
-CONTENT_STORE = "/Users/erik/Sites/static"
+#CONTENT_STORE = "/Users/erik/Sites/static"
+CONTENT_STORE = os.path.join(Globals.INSTANCE_HOME, 'var', 'eduintelligent')
